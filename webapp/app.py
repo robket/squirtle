daa4ab850424d9af2fedcc2115b5003ef3ab1e94
Robert Ketteringham <robket@gmail.com>
2013-05-15 11:35:08 +0200

    Database reads

--- webapp/app.py
+++ webapp/app.py
@@ -3,33 +3,35 @@ import datetime
 import simplejson
 from mako.template import Template
 from mako.lookup import TemplateLookup
+from datetime import datetime as dt
 
 class db:
-  def load(filename):
+  def load(self, filename):
     data = simplejson.load(open("data/"+filename))
     return data["records"]
 
-  def add(filename, record):
+  def add(self, filename, record):
     data = simplejson.load(open("data/"+filename))
     index = data["index"]
     data["records"][index + 1] = record
     simplejson.dump(data, open("data/"+filename, "w"))
 
-  def edit(filename, index, record):
+  def edit(self, filename, index, record):
     data = simplejson.load(open("data/"+filename))
     data["records"][index] = record
     simplejson.dump(data, open("data/"+filename, "w"))
 
-  def getLocations():
-    return load("locations")
+  def getLocations(self):
+    return self.load("locations")
 
-  def getParameters():
-    return load("parameters")
+  def getParameters(self):
+    return self.load("parameters")
 
-  def getRecords(location):
-    return load(location)
+  def getRecords(self, location):
+    return self.load(location)
 
 class squiver:
+    mydb = db()
     @cherrypy.expose
     def index(self):
         return "Welcome to squirtle system"
@@ -40,6 +42,38 @@ class squiver:
       date = datetime.date(int(year), int(month), int(day))
       data = {"year": year, "month": month, "day": day, "weekday": date.weekday()}
       return data 
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def from_db_monthly(self, location, year=2013):
+      params = self.mydb.getParameters()
+      outdict = {}
+      for param in params:
+        outdict[param["name"]] = []
+      for month in xrange(12):
+        data = self.from_db_daily(location, year, month)
+        for key in data.keys():
+          if key not in outdict:
+            continue
+          outdict[key].append(sum(data[key]) / 31.0)
+      return outdict
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def from_db_daily(self, location, year=2013, month=5):
+      params = self.mydb.getParameters()
+      data = self.mydb.getRecords(location)
+      outdict = {}
+      for param in params:
+        outdict[param["name"]] = [0]*31
+      for datum in data:
+        for key in datum["values"].keys():
+          if key not in outdict:
+            continue
+          ts = dt.fromtimestamp(datum["timestamp"])
+          if ts.year == year and ts.month == month:
+            outdict[key][ts.day] = datum["values"][key]
+      return outdict
     
     @cherrypy.expose
     def update(self, name="unknown"):
@@ -61,8 +95,9 @@ class squiver:
       #return mytemplate.render(first="foo", second="bar", third="baz")
       return mytemplate.render(first="foo", second="bar", third="baz", param1="A", param2="B", param3="C", param4="D")
 
+    # Stuff you get when someone submits
     @cherrypy.expose
-    def homeHandler(self, *args, **kwargs):
+    def addRecordHandler(self, *args, **kwargs):
       print kwargs
 
       s = ""
